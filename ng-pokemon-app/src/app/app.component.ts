fd2deaddc252c0c9cbc20a7e26d9dc3640e48d0a
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-19 16:15:17 +0300

    Variable referencer dans le template : SAVE 2:05

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -9,14 +9,24 @@ import { Pokemon } from "./pokemon";
 export class AppComponent implements OnInit {
   title = "Pokemon List";
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
     // this.selectPokemon(this.pokemonList[5]);
   }
 
-  selectPokemon(event: MouseEvent){
-    const index: number = +(event.target as HTMLInputElement).value;
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: string){
+    // const index: number = +(event.target as HTMLInputElement).value;
+    const id = +pokemonId;
+    const pokemon: Pokemon|undefined = this.pokemonList.find((pokemon => pokemon.id == +pokemonId));
+
+    if (pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon?.name}`);
+      this.pokemonSelected= pokemon;
+    }else{
+      console.log(`Vous avez démandé un pokemon qui n'existe pas  !`);
+      this.pokemonSelected= pokemon;
+    }
   }
 }
