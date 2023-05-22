a704cd85b0b02744af9f1b39dec1aa3f3db7b896
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-22 09:56:10 +0300

    Creer deux composant

---
+++ ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,16 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: [
+  ]
+})
+export class ListPokemonComponent implements OnInit {
+
+  constructor() {}
+  ngOnInit(): void {
+      
+  }
+}
