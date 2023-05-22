a3b0dbe3fcc4ae9c20dbd8bc93398f615f24fec4
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-22 09:27:52 +0300

    integrer le directive au composant html

--- ng-pokemon-app/src/app/border-card.directive.ts
+++ ng-pokemon-app/src/app/border-card.directive.ts
@@ -1,20 +1,29 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
-  selector: '[pokemonBorderCard]'
+  selector: "[pokemonBorderCard]",
 })
 export class BorderCardDirective {
-
   constructor(private el: ElementRef) {
     this.setHeight(180);
-    this.setBorder('#f5f5f5');
-   }
+    this.setBorder("#f5f5f5");
+  }
 
-  setHeight(height: number){
+  @Input('pokemonBorderCard') borderColor: string;
+
+  @HostListener("mouseenter") onMouseEnter() {
+    this.setBorder(this.borderColor || "#009688");
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.setBorder("#f5f5f5");
+  }
+
+  private setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
 
-  setBorder(color: string){
-    this.el.nativeElement.style.border = `solid 4x ${color}`;
+  private setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
   }
 }
