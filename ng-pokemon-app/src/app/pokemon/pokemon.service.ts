f35e8b3286e5cbdea030b7ebab8f321f9e2cc214
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-22 11:56:04 +0300

    Injecter un service propre seulement a un module

--- ng-pokemon-app/src/app/pokemon/pokemon.service.ts
+++ ng-pokemon-app/src/app/pokemon/pokemon.service.ts
@@ -3,9 +3,7 @@ import { Injectable } from "@angular/core";
 import { Pokemon } from "./pokemon";
 import { POKEMONS } from "./mock-pokemon-list";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable()
 export class PokemonService {
   getPokemonList(): Pokemon[] {
     return POKEMONS;
