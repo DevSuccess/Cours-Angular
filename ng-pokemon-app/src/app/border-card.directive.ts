5909acc13e509c20a1f20423e380be7aa32e972b
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-22 09:39:31 +0300

    Mise en place de Pipe

--- ng-pokemon-app/src/app/border-card.directive.ts
+++ ng-pokemon-app/src/app/border-card.directive.ts
@@ -4,19 +4,24 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
   selector: "[pokemonBorderCard]",
 })
 export class BorderCardDirective {
+
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
   constructor(private el: ElementRef) {
-    this.setHeight(180);
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
   }
 
   @Input('pokemonBorderCard') borderColor: string;
 
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder(this.borderColor || "#009688");
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
   }
 
   private setHeight(height: number) {
